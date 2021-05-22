e7f37560aebf877108bae91fde3205b5e6a923b9
Reshav Thakur <Reshavthkr@gmail.com>
2021-05-22 19:58:07 +0530

    event listener added

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,47 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
 
+
 class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
             renderBall: false,
             posi : 0,
-            ballPosition: { left: "0px" }
+            check:false,
+            ballPosition: { left: `0px` }
         };
         this.renderChoice = this.renderBallOrButton.bind(this)
         this.buttonClickHandler = this.buttonClickHandler.bind(this)
     };
 
     buttonClickHandler() {
-   
-   }
+        this.setState({renderBall: true})
+    }
     renderBallOrButton() {
 		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
+		    return <div className="ball"  style={this.state.ballPosition}></div>
 		} else {
 		    return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
 		}
     }
-
+   
     // bind ArrowRight keydown event
     componentDidMount() {
-      
+        
+            window.addEventListener('keydown', (event) => {
+                // console.log(event)
+                if(event.key==='ArrowRight'){
+                    console.log(true)
+                    this.setState({
+                        posi : this.state.posi+5,
+                        ballPosition : {left: this.state.posi+'px'}
+                    })
+                }
+              });
+            
+        
+    //   console.log(this.state.posi)
     }
 
     render() {
