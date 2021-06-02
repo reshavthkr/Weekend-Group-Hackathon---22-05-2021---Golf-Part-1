0e62b95c742b13402cb880cc1f506d9d6124f1da
Reshav Thakur <Reshavthkr@gmail.com>
2021-06-02 10:58:11 +0530

    Added key event

--- src/components/App.js
+++ src/components/App.js
@@ -13,32 +13,47 @@ class App extends Component {
         };
         this.renderChoice = this.renderBallOrButton.bind(this)
         this.buttonClickHandler = this.buttonClickHandler.bind(this)
+        // this.keyPressed = this.keyPressed.bind(this)
     };
+
+    // keyPressed(e)  {
+    //     if(e.keyCode == 39){
+    //                 // console.log(true)
+    //                 this.setState({
+    //                     posi : this.state.posi+5,
+    //                     ballPosition : {left: this.state.posi+'px'}
+    //                 })
+    //     }
+    // }
 
     buttonClickHandler() {
         this.setState({renderBall: true})
     }
     renderBallOrButton() {
 		if (this.state.renderBall) {
-		    return <div className="ball"  style={this.state.ballPosition}></div>
+		    return <div className="ball"   style={this.state.ballPosition} ></div>
 		} else {
-		    return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
+		    return <button onClick={this.buttonClickHandler}  >Click For One Ball</button>
 		}
     }
-   
+
     // bind ArrowRight keydown event
     componentDidMount() {
         
-            window.addEventListener('keydown', (event) => {
+
+
+
+
+            document.addEventListener('keydown', (event) => {
                 console.log(event)
-                if(event.key==='ArrowRight' || event.keyCode === 39){
+                if(event.key === "ArrowRight"){
                     console.log(true)
                     this.setState({
                         posi : this.state.posi+5,
                         ballPosition : {left: this.state.posi+'px'}
                     })
                 }
-              });
+            });
     }
 
     render() {
